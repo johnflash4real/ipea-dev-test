fd7dfa55165b2ac4de9ed5243480f23ec3143fa4
John Flash <johngood4real@yahoo.co.in>
2019-03-13 16:31:14 +0100

    function to load tags db

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Http\Helpers\InfusionsoftHelper;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use \App\User;
 use \App\Module;
+use App\Tag;
 use Response;
 
 class ApiController extends Controller
@@ -14,7 +16,9 @@ class ApiController extends Controller
     // Todo: Module reminder assigner
 
     /**
-     * Module reminder Assigner => Assigns reminders to user profile based on module progress
+     * Module reminder Assigner
+     *
+     * Assigns reminders to user profile based on module progress
      *
      * @param Request $request
      * @param InfusionsoftHelper $infusionsoftHelper
@@ -30,14 +34,39 @@ class ApiController extends Controller
 
         //get contact with email or return failure response
         $userEmail = $request->input('contact_email');
-        $user = User::where('email',$userEmail)->first();
+        $user = User::where('email',$userEmail)->with('completed_modules')->first();
         if(!$user) return response()->json(['success'=>false,'message'=>'user not found'],404);
 
+        $tags = $this->getTags();
+
+        //fetch user's modules
+        //$userCompletedModules = $user->completed_modules;
 
 
 
 
-        return response()->json(['success'=>true,'message'=>'ok','user'=>$user]);
+        return response()->json(['success'=>true,'message'=>'ok','user'=>$user,'tags'=>$tags]);
+    }
+
+
+    //check db for tags or fetch from api then save in db
+    private function getTags(){
+        $tags = Tag::all(); //load all from db
+
+        if(count($tags)==0){
+            //if no tag in db, load into db from infusionsoft
+            $infusionsoftHelper = new InfusionsoftHelper();
+            $allTags = $infusionsoftHelper->getAllTags()->all();
+            $tagsArray = [];
+            $now = Carbon::now()->toDateTimeString();
+            foreach ($allTags as $tag)
+                $tagsArray[] = ['tag_id'=>$tag->id,'name'=>$tag->name,'created_at'=>$now,'updated_at'=>$now];
+
+            Tag::insert($tagsArray);
+            $tags = Tag::all();
+        }
+
+        return $tags;
     }
 
     /**
--- app/Tag.php
+++ app/Tag.php
@@ -7,4 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Tag extends Model
 {
 
+    protected $fillable = ['tag_id','name','created_at','updated_at'];
 }
